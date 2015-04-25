deaac8cf5e99190f5c95063c1a396ff591e465d3
Theodore Kimble <mail@theodorekimble.com>
2015-04-24 18:08:35 -0700

    Add links to existing media manager image placements

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -11,6 +11,46 @@ class Placement < ActiveRecord::Base
     ImageAttachmentReprocessJob.perform_later(image) if image && image_id_changed?
   end
 
+  def placer_location
+    case context
+    when 'logo'
+      Rails.application.routes.url_helpers.edit_business_data_details_path(placer)
+    when 'team_member_profile'
+      Rails.application.routes.url_helpers.edit_business_data_team_member_path(placer.business, placer)
+    when 'about_block_image'
+      placer_block_location
+    when 'hero_block_image'
+      placer_block_location
+    when 'call_to_action_block_image'
+      placer_block_location
+    when 'content_block_image'
+      placer_block_location
+    when 'specialty_block_image'
+      placer_block_location
+    when 'before_image'
+      Rails.application.routes.url_helpers.edit_business_content_before_after_path(placer.business, placer)
+    when 'after_image'
+      Rails.application.routes.url_helpers.edit_business_content_before_after_path(placer.business, placer)
+    when 'gallery_image'
+      Rails.application.routes.url_helpers.edit_business_content_gallery_path(placer.gallery.business, placer.gallery)
+    end
+  end
+
+  def placer_block_location
+    case placer.frame.type
+    when 'HomePage'
+      Rails.application.routes.url_helpers.edit_business_website_home_page_path(placer.frame.website.business, placer.frame)
+    when 'AboutPage'
+      Rails.application.routes.url_helpers.edit_business_website_about_page_path(placer.frame.website.business, placer.frame)
+    when 'BlogPage'
+      Rails.application.routes.url_helpers.edit_business_website_blog_page_path(placer.frame.website.business, placer.frame)
+    when 'ContactPage'
+      Rails.application.routes.url_helpers.edit_business_website_contact_page_path(placer.frame.website.business, placer.frame)
+    when 'CustomPage'
+      Rails.application.routes.url_helpers.edit_business_website_custom_page_path(placer.frame.website.business, placer.frame)
+    end
+  end
+
   def styles
     context? && respond_to?(context) ? send(context) : {}
   end
