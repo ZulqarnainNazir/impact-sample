4bf97ff0e9b19f5a0386de0a858956d1b1abf7ec
Theodore Kimble <mail@theodorekimble.com>
2015-06-03 12:29:33 -0700

    Allow super users to browse all images in block media library

--- app/controllers/businesses/images_controller.rb
+++ app/controllers/businesses/images_controller.rb
@@ -2,7 +2,7 @@ class Businesses::ImagesController < Businesses::BaseController
   def index
     @images = Image.
       includes(:placements).
-      where(where_clause).
+      send(*where_clause).
       order(created_at: :desc).
       page(params[:page]).
       per(48)
@@ -12,9 +12,11 @@ class Businesses::ImagesController < Businesses::BaseController
 
   def where_clause
     if params[:local] == 'true'
-      ['business_id = ?', @business.id]
+      [:where, ['business_id = ?', @business.id]]
+    elsif current_user.super_user?
+      [:all]
     else
-      ['business_id = ? OR user_id = ?', @business.id, current_user.id]
+      [:where, ['business_id = ? OR user_id = ?', @business.id, current_user.id]]
     end
   end
 end
