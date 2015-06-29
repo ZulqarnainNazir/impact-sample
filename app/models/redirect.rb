c1792a17b4933ccf8432a60371e2da768176da7b
Theodore Kimble <mail@theodorekimble.com>
2015-06-29 10:11:15 -0700

    Fix for trailing slash in redirect origin
    [Delivers #97677260]

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -6,7 +6,12 @@ class Redirect < ActiveRecord::Base
   validates :to_path, format: { with: /\A((\/|http\:\/\/|https\:\/\/))(([^\/]+.*)|(.{0}))\z/ }
 
   before_validation do
+    # Add leading front-slash:
     self.from_path = "/#{from_path}" if !from_path.to_s.match(/\A\//)
     self.to_path = "/#{to_path}" if !to_path.to_s.match(/\A((\/|http\:\/\/|https\:\/\/))/)
+
+    # Remove trailing front-slash:
+    self.from_path = from_path.sub(/\/\z/, '') unless from_path.length == 1
+    self.to_path = to_path.sub(/\/\z/, '') unless to_path.length == 1
   end
 end
