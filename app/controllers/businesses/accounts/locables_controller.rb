feaa13f11c46135ab6972fb907fceb77bc22a012
Theodore Kimble <mail@theodorekimble.com>
2015-07-06 13:02:03 -0700

    Switch to claim_link and create_claim_link locable methods

--- app/controllers/businesses/accounts/locables_controller.rb
+++ app/controllers/businesses/accounts/locables_controller.rb
@@ -1,5 +1,5 @@
 class Businesses::Accounts::LocablesController < Businesses::BaseController
-  before_action do
+  def edit
     @locable_businesses_json = Array(connect_to('/businesses', email: current_user.email)[:businesses])
     @locable_sites_json = Array(connect_to('/sites')[:sites])
   end
@@ -14,20 +14,19 @@ class Businesses::Accounts::LocablesController < Businesses::BaseController
         redirect_to [:edit, @business, :accounts_locable], alert: 'There was a problem linking your Locable account.'
       end
     elsif params[:add_to_locable].present?
-      payload = connect_to('/businesses/claim', email: current_user.email, site_id: params[:site_id], business: @business.as_json(include: { location: {}, categories: { only: :name }}))
+      payload = connect_to('/businesses/create_claim_link', email: current_user.email, site_id: params[:site_id], business: @business.as_json(include: { location: {}, categories: { only: :name }}))
       if payload[:id] && @business.update(cce_id: payload[:id], cce_name: payload[:name], cce_url: payload[:locable_url])
         redirect_to [:edit, @business, :accounts_locable], notice: 'Your Locable listing was successfully claimed.'
       else
         redirect_to [:edit, @business, :accounts_locable], alert: "There was a problem claiming your Locable listing – #{payload[:message]}"
       end
     else
-      locable_slug = params[:locable_url].split('/').last
-      payload = connect_to("/businesses/#{locable_slug}", email: current_user.email)
+      locable_slug = params[:locable_url].split('/').last.parameterize
+      payload = connect_to("/businesses/#{locable_slug}/claim_link", id: @business.id, email: current_user.email)
       if payload[:id] && @business.update(cce_id: payload[:id], cce_name: payload[:name], cce_url: payload[:locable_url])
-        connect_to "/businesses/#{@business.cce_id}/link", impact_id: @business.id
         redirect_to [:edit, @business, :accounts_locable], notice: 'Your Locable account was successfully linked.'
       else
-        redirect_to [:edit, @business, :accounts_locable], alert: 'Sorry, it looks like that page is missing or has a privacy setting that prevents us from importing data.'
+        redirect_to [:edit, @business, :accounts_locable], alert: "There was a problem claiming your Locable listing – #{payload[:message]}"
       end
     end
   end
