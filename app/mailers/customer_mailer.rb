0c64ad187b8d6a6515ac3e512fc92f4444d9ca2a
Theodore Kimble <mail@theodorekimble.com>
2015-07-13 11:55:11 -0700

    Update feedback email

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -5,6 +5,6 @@ class CustomerMailer < ApplicationMailer
     @feedback = feedback
     @business = feedback.customer.business
     @website_host = website_host(@business.website)
-    mail to: @feedback.customer.email
+    mail to: @feedback.customer.email, subject: "Feedback on your experience with #{@business.name}"
   end
 end
