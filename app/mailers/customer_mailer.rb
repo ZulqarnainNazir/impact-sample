7ec19126ef697fb03a4fa716a9f37a43fb1b232d
Theodore Kimble <mail@theodorekimble.com>
2015-08-13 10:48:38 -0700

    Customize customer mailer from address

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -6,7 +6,7 @@ class CustomerMailer < ApplicationMailer
       @feedback = feedback
       @business = feedback.customer.business
       @website_host = website_host(@business.website)
-      mail to: @feedback.customer.email, subject: "Feedback on your experience with #{@business.name}"
+      mail to: @feedback.customer.email, from: "#{@business.name} <#{Rails.application.secrets.support_email}>", subject: "Feedback on your experience with #{@business.name}"
     end
   end
 end
