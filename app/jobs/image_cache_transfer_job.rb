023a655b0f73e8bf89e38fb73449528d22c20f68
Theodore Kimble <mail@theodorekimble.com>
2015-09-29 10:36:01 -0700

    Update image cache transfer job to handle brackets
    [Delivers #104102978]

--- app/jobs/image_cache_transfer_job.rb
+++ app/jobs/image_cache_transfer_job.rb
@@ -3,7 +3,7 @@ class ImageCacheTransferJob < ApplicationJob
 
   def perform(image)
     if image.attachment_cache_url?
-      url = image.attachment_cache_url.gsub(/\s/, '+')
+      url = image.attachment_cache_url.gsub(/\s/, '+').gsub('[', '%5B').gsub(']', '%5D')
       url = "http:#{url}" if url.match(/\A\/\//)
       image.update!(attachment: URI.parse(url), attachment_cache_url: nil)
     end
