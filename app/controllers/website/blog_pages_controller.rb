b5627db96bab21d14aade8031dfca7bf5084a347
Taylor Barnette <taylor.a.barnette@gmail.com>
2016-08-07 21:24:39 -0400

    8.7.16 -3

--- app/controllers/website/blog_pages_controller.rb
+++ app/controllers/website/blog_pages_controller.rb
@@ -1,5 +1,6 @@
 class Website::BlogPagesController < Website::BaseController
   before_action do
     @page = @website.blog_page or raise ActiveRecord::RecordNotFound
+    @masonry = true
   end
 end
