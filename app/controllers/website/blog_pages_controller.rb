159832e685b4ac39728e2e00946c0c1cc34d0a81
Taylor Barnette <taylor.a.barnette@gmail.com>
2016-08-14 12:58:52 -0400

    8.14.16 -1

--- app/controllers/website/blog_pages_controller.rb
+++ app/controllers/website/blog_pages_controller.rb
@@ -1,10 +1,12 @@
 class Website::BlogPagesController < Website::BaseController
   before_action do
     @page = @website.blog_page or raise ActiveRecord::RecordNotFound
-    @content_types = @page.groups.where(type:"BlogFeedGroup").first.blocks.first.content_types.split
+    @content_types_all = @page.groups.where(type:"BlogFeedGroup").first.blocks.first.content_types.split
     @masonry = true
     if !params[:content_types].present?
-    	params[:content_types] = @content_types
+    	params[:content_types] = @content_types_all
+    elsif params[:content_types].present?
+    	@content_types = params[:content_types]
     end
   end
 end
