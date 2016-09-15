e07a66c895c1ae6298cc3ba17040e837453dda4b
ccslakey <cromwellslakey@gmail.com>
2016-09-15 12:20:51 -0700

    footer embed options for blog posts and landing pages

--- db/migrate/20160915183628_add_restrictions_to_website_footer_embed.rb
+++ db/migrate/20160915183628_add_restrictions_to_website_footer_embed.rb
@@ -1,6 +1,11 @@
 class AddRestrictionsToWebsiteFooterEmbed < ActiveRecord::Migration
   def change
+<<<<<<< 9fd7e96272fac9e3251475084c01fb12ddf51620
     add_column :websites, :hide_on_landing, :boolean
     add_column :websites, :hide_on_blog, :boolean
+=======
+    add_column :websites, :embed_on_landing, :boolean
+    add_column :websites, :embed_on_blog, :boolean
+>>>>>>> footer embed options for blog posts and landing pages
   end
 end
