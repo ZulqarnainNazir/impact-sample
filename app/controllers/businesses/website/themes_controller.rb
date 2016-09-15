6565bdc10cd2044a4a0a6b03a65a766c0fe1303c
ccslakey <cromwellslakey@gmail.com>
2016-09-15 14:39:16 -0700

    rebase gods above

--- app/controllers/businesses/website/themes_controller.rb
+++ app/controllers/businesses/website/themes_controller.rb
@@ -5,6 +5,7 @@ class Businesses::Website::ThemesController < Businesses::Website::BaseControlle
   layout 'application'
 
   def update
+    binding.pry
     update_resource @website, website_params, location: [:edit, @business, :website_theme]
   end
 
@@ -17,8 +18,11 @@ class Businesses::Website::ThemesController < Businesses::Website::BaseControlle
       :foreground_color,
       :link_color,
       :footer_embed,
+<<<<<<< 65fafb1107bcea5f6c4686d912853935a7da8605
       :embed_on_blog,
       :embed_on_landing,
+=======
+>>>>>>> basic text form for footer embed: need to finish by adding it to the layout and restricting where it appears
       :wrap_container,
       header_block_attributes: block_attributes.push(*%i[logo_height logo_horizontal_position logo_vertical_position navigation_horizontal_position contact_position navbar_location]),
       footer_block_attributes: block_attributes,
