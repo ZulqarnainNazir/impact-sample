dd4d4aa3aa00d6ba1fc36b96a22322f2df85b3ae
ccslakey <cromwellslakey@gmail.com>
2016-09-16 10:49:54 -0700

    missed something in 20160915183628_add_restrictions_to_website_footer_embed

--- db/migrate/20160915183628_add_restrictions_to_website_footer_embed.rb
+++ db/migrate/20160915183628_add_restrictions_to_website_footer_embed.rb
@@ -1,16 +1,6 @@
 class AddRestrictionsToWebsiteFooterEmbed < ActiveRecord::Migration
   def change
-<<<<<<< ea946d7e6dab96d4a564af6a77058724664d350a
-<<<<<<< 9fd7e96272fac9e3251475084c01fb12ddf51620
     add_column :websites, :hide_on_landing, :boolean
     add_column :websites, :hide_on_blog, :boolean
-=======
-    add_column :websites, :embed_on_landing, :boolean
-    add_column :websites, :embed_on_blog, :boolean
->>>>>>> footer embed options for blog posts and landing pages
-=======
-    add_column :websites, :hide_on_landing, :boolean
-    add_column :websites, :hide_on_blog, :boolean
->>>>>>> fixed logic to hide footer embed: next - change website builder
   end
 end
