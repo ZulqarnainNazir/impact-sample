f6fc33d741d6d9cb89c8a7b50be0f60b43ca90e1
ccslakey <cromwellslakey@gmail.com>
2016-09-19 11:12:08 -0700

    stupid devs love pry

--- app/controllers/businesses/website/themes_controller.rb
+++ app/controllers/businesses/website/themes_controller.rb
@@ -6,7 +6,6 @@ class Businesses::Website::ThemesController < Businesses::Website::BaseControlle
 
   def update
     update_resource @website, website_params, location: [:edit, @business, :website_theme]
-    binding.pry
   end
 
   private
