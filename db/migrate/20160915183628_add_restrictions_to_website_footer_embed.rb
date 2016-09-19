0a2660a39897ceb925755c30ddff657727ebfdcc
ccslakey <cromwellslakey@gmail.com>
2016-09-19 10:57:08 -0700

    more migration fixes

--- db/migrate/20160915183628_add_restrictions_to_website_footer_embed.rb
+++
@@ -1,6 +0,0 @@
-class AddRestrictionsToWebsiteFooterEmbed < ActiveRecord::Migration
-  def change
-    add_column :websites, :hide_on_landing, :boolean
-    add_column :websites, :hide_on_blog, :boolean
-  end
-end
--- db/migrate/20160918232637_remove_restrictions_from_website_footer_embed.rb
+++
@@ -1,6 +0,0 @@
-class RemoveRestrictionsFromWebsiteFooterEmbed < ActiveRecord::Migration
-  def change
-    remove_column :websites, :hide_on_landing, :boolean
-    remove_column :websites, :hide_on_blog, :boolean
-  end
-end
