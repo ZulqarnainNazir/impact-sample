8318aa620d55f5f4eb2f0ebffac392fbfb861ac2
ccslakey <cromwellslakey@gmail.com>
2016-09-20 14:58:35 -0700

    new migrations for pdf attachment field - form needs work
    remove binding.prys
    
    remove spare binding.pry
    
    such confusion
    
    basic JS for ajax uploads - needs flash error/notice?
    
    javascript for first time pdf upload
    
    stop messing around with pdf.rb
    
    change attr_accessor
    
    more paperclip attr_accessors
    
    add controller params
    
    removed unecessary attr_accessors
    
    finishing tweaks for now
    
    Replaces glyphicon w Font Awesome
    
    sitemaps for client websites: remove from noind/nofollow pages next
    
    cron job for sitemaps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-iveRecord::Schema.define(version: 20160927210402) do
+ActiveRecord::Schema.define(version: 20160927210402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
