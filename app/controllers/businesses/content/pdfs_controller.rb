2d2100a28c08b01aaf3e7ecbe0b93f35047677b1
ccslakey <cromwellslakey@gmail.com>
2016-09-20 14:58:35 -0700

    new migrations for pdf attachment field - form needs work

--- app/controllers/businesses/content/pdfs_controller.rb
+++ app/controllers/businesses/content/pdfs_controller.rb
@@ -5,19 +5,21 @@ class Businesses::Content::PdfsController < Businesses::Content::BaseController
     else
       params[:local] = 'true'
     end
-
+    @business = Business.find(params['business_id'])
     if params[:local] == 'true'
-      # binding.pry
-      @business = Business.find(params['business_id'])
+      binding.pry
       @pdfs = Pdf.where(business_id: @business.id).order(created_at: :desc).page(params[:page]).per(48)
+      binding.pry
     else
+      binding.pry
       @pdfs = Pdf.where('business_id = ? OR user_id = ?', @business.id, current_user.id).order(created_at: :desc).page(params[:page]).per(48)
+      binding.pry
     end
   end
 
   def new
     @business = Business.find(params['business_id'])
-    @pdf = Pdf.new
+    @pdf = @business.pdfs.new(user: current_user)
   end
 
   def create
@@ -25,6 +27,10 @@ class Businesses::Content::PdfsController < Businesses::Content::BaseController
     update_resource @pdf, pdf_params
   end
 
+  def show
+    @business = Business.find(params[:business_id])
+    @pdf = Pdf.find(params[:id])
+  end
 
   private
 
@@ -32,6 +38,7 @@ class Businesses::Content::PdfsController < Businesses::Content::BaseController
     params.require(:pdf).permit(
       :file_name,
       :file_size,
+      :attachment
     )
   end
 end
