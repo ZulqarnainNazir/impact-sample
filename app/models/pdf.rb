e2037a051d6fe0ce4719e4409f6e6dc4a6dffcf2
ccslakey <cromwellslakey@gmail.com>
2016-09-21 14:58:40 -0700

    errors, validations, migrations, form, and index page for pdfs

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,7 +1,10 @@
 class Pdf < ActiveRecord::Base
   belongs_to :user
   belongs_to :business
+<<<<<<< b0c1514e4796a757b3833f7f930569800e7c5d21
 <<<<<<< 65521e64327587f369d95bc14f61b098a0b44919
+=======
+>>>>>>> errors, validations, migrations, form, and index page for pdfs
   attr_accessor :attachment
   has_attached_file :attachment
 
@@ -11,8 +14,11 @@ class Pdf < ActiveRecord::Base
   validates_with AttachmentPresenceValidator, attributes: :attachment
   validates_with AttachmentSizeValidator, attributes: :attachment, less_than: 20.gigabytes
 
+<<<<<<< b0c1514e4796a757b3833f7f930569800e7c5d21
 =======
 >>>>>>> skeleton for pdfs - navigation, controller, model
+=======
+>>>>>>> errors, validations, migrations, form, and index page for pdfs
 
   validates :business_id, presence: true, unless: :business
   validates :user_id, presence: true, unless: :user
--- db/migrate/20160920195843_create_pdfs.rb
+++ db/migrate/20160920195843_create_pdfs.rb
@@ -1,13 +1,9 @@
 class CreatePdfs < ActiveRecord::Migration
   def change
     create_table :pdfs do |t|
-<<<<<<< e89d73c4a6c09dc7135b2adf290248839e135188
       t.attachment :attachment
-=======
       t.string :file_name
       t.string :file_size
-      t.string :attachment
->>>>>>> new migrations for pdf attachment field - form needs work
       t.references :user, index: true, foreign_key: true
       t.references :business, index: true, foreign_key: true
 
