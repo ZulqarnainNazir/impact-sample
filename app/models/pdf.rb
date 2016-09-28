d8525d85d9a5985e7544883ac85067fead5e40af
ccslakey <cromwellslakey@gmail.com>
2016-09-28 15:42:04 -0700

    stop messing around with pdf.rb

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -2,6 +2,8 @@ class Pdf < ActiveRecord::Base
   belongs_to :user
   belongs_to :business
   attr_accessor :attachment
+  # attr_accessor :attachment_file_name
+
   has_attached_file :attachment
 
   validates_attachment_file_name :attachment, :matches => [/png\Z/, /jpe?g\Z/, /pdf\Z/]
