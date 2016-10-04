23dd92c2579f59b0578589a42f9c96a66bfe6aa7
ccslakey <cromwellslakey@gmail.com>
2016-10-04 11:57:36 -0700

    respect admin/client robot.txt differences

--- app/controllers/website/robots_controller.rb
+++ app/controllers/website/robots_controller.rb
@@ -1,6 +1,10 @@
 class Website::RobotsController < Website::BaseController
 
   def txt
+    if current_user
+      robots = File.read(Rails.root + "public/robots.#{Rails.env}.txt")
+      render :text => robots, :layout => false, :content_type => "text/plain"
+    end
     respond_to :text
     expires_in 6.hours, public: true
     # render 'views/website/robots/txt.text.slim'
