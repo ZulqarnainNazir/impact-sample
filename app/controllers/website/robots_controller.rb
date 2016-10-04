d4d4ae0e1b4396dad9a8c6cc6d604e58ebddc331
ccslakey <cromwellslakey@gmail.com>
2016-10-04 12:29:14 -0700

    admin robots.txt

--- app/controllers/website/robots_controller.rb
+++ app/controllers/website/robots_controller.rb
@@ -2,11 +2,10 @@ class Website::RobotsController < Website::BaseController
 
   def txt
     if current_user
-      robots = File.read(Rails.root + "public/robots.txt")
+      robots = File.read(Rails.root + "public/robots.admin.txt")
       render :text => robots, :layout => false, :content_type => "text/plain"
     end
     respond_to :text
     expires_in 6.hours, public: true
-    # render 'views/website/robots/txt.text.slim'
   end
 end
