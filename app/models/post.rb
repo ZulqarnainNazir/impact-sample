4b7e914d236c2ba0276d6498c649b9845cdcee36
ccslakey <cromwellslakey@gmail.com>
2016-10-11 10:33:21 -0700

    better edit datetime for content published on, client blog fix for related issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -112,9 +112,9 @@ class Post < ActiveRecord::Base
 
   def to_generic_param
     {
-      year: published_on.strftime('%Y'),
-      month: published_on.strftime('%m'),
-      day: published_on.strftime('%d'),
+      year: published_at.strftime('%Y'),
+      month: published_at.strftime('%m'),
+      day: published_at.strftime('%d'),
       id: id,
       slug: slug,
     }
