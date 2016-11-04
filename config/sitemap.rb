8d5f4ceafc6ef9b2a836f876f611d0af7ea53add
brianmba <brian@locable.com>
2016-11-04 07:48:05 -0700

    92463614 add sitemaps (#27)
    * still need this branch
    
    * correct s3 w/o localization
    
    * more s3 path refinement
    
    * remove aggressive returns in favor of if statements
    
    * next is more idiomatic

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -11,7 +11,7 @@ SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['AWS_S3_BUCKET']}.s3-#{E
 
 Business.find_each do |business|
   website = business.try(:website)
-  return if website.nil?
+  next if website.nil?
 
   webhost = website.webhosts.try(:find_by, :primary => true).try(:name)
   if webhost.nil?
@@ -33,47 +33,45 @@ Business.find_each do |business|
     add website_reviews_path(website), :lastmod => website.updated_at unless website.blog_page.try(:settings).try(:no_index)
     add new_website_review_path(website), :lastmod => website.updated_at unless website.blog_page.try(:settings).try(:no_index)
 
-    return if website.business.nil?
-
     website.business.before_afters.find_each do |before_after|
-      return if website.business.before_afters.empty?  
+      next if website.business.before_afters.empty?  
       add website_before_after_path(before_after), :lastmod => before_after.updated_at
     end
 
     website.custom_pages.find_each do |page|
-      return if website.custom_pages.empty?
+      next if website.custom_pages.empty?
       add website_custom_page_path(page), :lastmod => page.updated_at
     end
 
     website.business.categories.find_each do |content_category|
-      return if website.business.categories.empty?
+      next if website.business.categories.empty?
       add website_content_category_path(content_category), :lastmod => content_category.updated_at
     end
     website.business.events.find_each do |event|
-      return if website.business.events.empty?  
+      next if website.business.events.empty?  
       add website_event_path(event), :lastmod => event.updated_at
     end
     website.business.offers.find_each do |offer|
-      return if website.business.offers.empty?  
+      next if website.business.offers.empty?  
       add website_offer_path(offer), :lastmod => offer.updated_at
     end
     website.business.posts.find_each do |post|
-      return if website.business.posts.empty?  
+      next if website.business.posts.empty?  
       add website_post_path(post), :lastmod => post.updated_at
     end
     website.business.quick_posts.find_each do |quick_post|
-      return if website.business.quick_posts.empty?  
+      next if website.business.quick_posts.empty?  
       add website_quick_post_path(quick_post), :lastmod => quick_post.updated_at
     end
     website.business.reviews.find_each do |review|
-      return if website.business.reviews.empty?  
+      next if website.business.reviews.empty?  
       add website_review_path(review), :lastmod => review.updated_at
     end
     website.business.galleries.find_each do |gallery|
-      return if website.business.galleries.empty?  
+      next if website.business.galleries.empty?  
       add website_gallery_path(gallery), :lastmod => gallery.updated_at
       gallery.gallery_images.find_each do |image|
-        return if gallery.gallery_images.empty?  
+        next if gallery.gallery_images.empty?  
         add website_gallery_gallery_image_path(gallery, image), :lastmod => image.updated_at
       end
     end
