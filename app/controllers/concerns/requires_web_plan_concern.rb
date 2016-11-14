0c78c69ee74048e09b1e6457433696c647409ae1
Philip Speake <philip_speake@yahoo.com>
2016-11-14 15:49:40 -0800

    merge conflicts

--- app/controllers/concerns/requires_web_plan_concern.rb
+++ app/controllers/concerns/requires_web_plan_concern.rb
@@ -2,10 +2,10 @@ module RequiresWebPlanConcern
   extend ActiveSupport::Concern
 
   included do
-    # before_action do
-    #   if @business.free?
-    #     redirect_to [@business, :dashboard], alert: 'Please Upgrade to a Web or Primary subscription to access that feature.'
-    #   end
-    # end
+    before_action do
+      if @business.free?
+        redirect_to [@business, :dashboard], alert: 'Please Upgrade to a Web or Primary subscription to access that feature.'
+      end
+    end
   end
 end
