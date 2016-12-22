37c0061779eea06915dcdc3369bda349cb0ea249
Chris Selmer <cmselmer@gmail.com>
2016-12-22 14:34:06 -0800

    Switch to Lambda-based resizing
    - Non-Paperclip validations for size, content type, cache url
    - Remove hooks to cache transfer job
    - Add tests and fixtures for image
    - Remove Paperclip references

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -24,7 +24,7 @@ class ImageTest < ActiveSupport::TestCase
   test '#attachment_url correctly translates original file name to styled version' do
     image = images(:valid_image)
     image.attachment_cache_url = 'http://locable.com/_originals/123/logo.png'
-
+    
     assert_equal 'http://locable.com/r/big/123/logo.png', image.attachment_url('big')
   end
 end
