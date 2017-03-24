ead993ff2a5a27f2b2f43e58e373c48b843a4c84
Taylor Barnette <taylor.a.barnette@gmail.com>
2017-03-24 14:45:45 -0400

    hotfix: change description to content two migration

---
+++ db/migrate/20170324184506_change_description_to_content_two.rb
@@ -0,0 +1,5 @@
+class ChangeDescriptionToContentTwo < ActiveRecord::Migration
+  def change
+  	rename_column(:quick_posts, :description, :content) unless QuickPost.column_names.include?('content')
+  end
+end
