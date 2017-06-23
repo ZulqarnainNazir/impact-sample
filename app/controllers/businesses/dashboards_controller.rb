df23d858b7fa4a9430f5ec20db6401fcf4fb1d63
brianmba <brian@locable.com>
2017-06-22 17:36:46 -0700

    Adjust dashboard (#312)
    * Reorders Dash and adds prompts
    
    * Adjustments
    
    * Update import language
    
    * Update colors
    
    * More adjustments
    
    * Icons
    
    * Reorder at Plan
    
    * First cut of update
    
    * WIP Adjustment
    
    * WIP Missions
    
    * Browse
    
    * Truncate mission content
    
    * Typo
    
    * fixes syntax error for title truncate
    
    * Updates copy
    
    * fixes issue with '# in network' display
    
    * adds mission count
    
    * Consolidates view more 
    
    Still need to move away from UL
    
    * Copy typo
    
    * Moves website todos up ahead of consolidation
    
    * Changed to first three
    
    * revert
    
    * adds mission instance count to dashboard card
    
    * Change reviews link
    
    * fixes disagreement on mission count between activity feed and missions card

--- app/controllers/businesses/dashboards_controller.rb
+++ app/controllers/businesses/dashboards_controller.rb
@@ -4,4 +4,12 @@ class Businesses::DashboardsController < Businesses::BaseController
       redirect_to :new_onboard_website_business
     end
   end
+
+  def show
+    params[:future] ||= true unless params[:past]
+    @events = ActivityCalendar.new(business: @business).timeline_events(nil, params[:future], params[:past]) #.limit(2)
+    @event_count = @events.size
+    @events = @events.limit(2)
+    @mission_instances = @business.mission_instances
+  end
 end
