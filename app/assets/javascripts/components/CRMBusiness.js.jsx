323d41aceecc505aeb9aae7ccbbf4db321a16ed8
brianmba <brian@locable.com>
2018-08-24 12:43:27 -0700

    Messaging appcues (#501)
    * Copy Change
    
    * Adds appcues
    
    * Adds appcues to footer
    
    * syntax
    
    * Formatting
    
    * Copy
    
    * Reorders to group contacts
    
    * Adds conditional to embedded calendar
    
    * Roll back
    
    Should this be conditional?
    
    * Comments out share buttons
    
    * Comments out share buttons
    
    * Comments out share buttons
    
    * Comments out share buttons
    
    * Comments out share buttons
    
    * Comments out share buttons
    
    * Comments out share buttons
    
    * Adjusts order and copy
    
    * Reorders and copy
    
    * More adjustments
    
    * Comment out button
    
    * Removes phone
    
    * More copy
    
    * Updates Jump to labels
    
    * Updates copy
    
    * Updates Copy
    
    * Adds placeholder
    
    * Adds placeholder
    
    * Moves message
    
    * Syntax
    
    * Copy updates
    
    * Updates copy
    
    * Copy

--- app/assets/javascripts/components/CRMBusiness.js.jsx
+++ app/assets/javascripts/components/CRMBusiness.js.jsx
@@ -7,12 +7,11 @@ CRMBusiness = ({ id, href, name, location, onClick, disabled }) => (
           <p className="m-b-none">{location.street1}</p>
           <p className="m-b-none">{location.street2}</p>
           <p className="m-b-none">{location.city && location.state ? `${location.city}, ${location.state} ${location.zip_code}` : ''}</p>
-          <p className="m-b-none">{location.phone_number}</p>
         </address>
       </a>
       <div className="contact-box-footer">
         <a href={href} data-method={disabled ? "get" : "post"} onClick={onClick} className="btn btn-primary btn-block">{ disabled ? "View it now" : "This is it"}</a>
-        <label className="label-sm label-warning">{disabled ? "Already a Part of your CRM" : ""}</label>
+        <label className="label-sm label-warning">{disabled ? "Already in Companies & Causes" : ""}</label>
       </div>
     </div>
   </div>
