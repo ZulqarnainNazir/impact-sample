7246837a66a357354121bede8000fa6817dd9cbe
Ryan Frisch <ryan@locable.com>
2019-03-04 11:07:04 -0800

    Add callback to set installation status to true when reach is recorded

--- app/models/ahoy/event.rb
+++ app/models/ahoy/event.rb
@@ -6,4 +6,22 @@ class Ahoy::Event < ActiveRecord::Base
   belongs_to :visit
   belongs_to :user
 
+  after_save :set_widget_status
+
+  private
+    def set_widget_status
+      if self.properties['type'] == 'Content Widget'
+        w = ContentFeedWidget.find(self.properties['type_id'])
+        w.status = true
+        w.save
+      elsif self.properties['type'] == 'Calendar Widget'
+        w = CalendarWidget.find(self.properties['type_id'])
+        w.status = true
+        w.save
+      elsif self.properties['type'] == 'Directory Widget'
+        w = DirectoryWidget.find(self.properties['type_id'])
+        w.status = true
+        w.save
+      end
+    end
 end
