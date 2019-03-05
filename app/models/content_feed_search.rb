44166f5e534ff35dc414e664a93f7af747c5934f
Creation-Station <seanebeighton@gmail.com>
2019-03-04 21:51:30 -0800

    fix query generator

--- app/models/content_feed_search.rb
+++ app/models/content_feed_search.rb
@@ -26,6 +26,9 @@ class ContentFeedSearch
             term: {
               business_id: @business.id,
             },
+            term: {
+              published_status: true,
+            },
           },
         ],
       },
@@ -33,7 +36,7 @@ class ContentFeedSearch
     if @unpublished == 'true' && @published != 'true'
       dsl1[:filter][:and] << {
         term: {
-          published_status: false,
+          published_status: (false || nil),
         },
       }
     end
@@ -41,7 +44,7 @@ class ContentFeedSearch
     if @unpublished != 'true' && @published == 'true'
       dsl1[:filter][:and] << {
         term: {
-          published_status: !false,
+          published_status: true,
         },
       }
     end
@@ -95,7 +98,7 @@ class ContentFeedSearch
     if @unpublished == 'true' && @published != 'true'
       dsl2[:filter][:and] << {
         term: {
-          published_status: false,
+          published_status: (false || nil),
         },
       }
     end
@@ -103,7 +106,7 @@ class ContentFeedSearch
     if @unpublished != 'true' && @published == 'true'
       dsl2[:filter][:and] << {
         term: {
-          published_status: !false,
+          published_status: true,
         },
       }
     end
@@ -132,14 +135,14 @@ class ContentFeedSearch
               multi_match: {
                 fields: %w[title^2 description content heading],
                 query: @query
-              }   
+              }
             },
             {
               nested: {
                 path: "post_sections",
                 query: {
                   multi_match: {
-                    fields: ["post_sections.content", "post_sections.heading"], 
+                    fields: ["post_sections.content", "post_sections.heading"],
                     query: @query
                   }
                 }
@@ -210,14 +213,14 @@ class ContentFeedSearch
 
     # elsif !@query.blank?
     #   #if the user has specified a query string, and has specified a content_type,
-    #   #and that content_type includes Post, then execute TWO queries. The first 
+    #   #and that content_type includes Post, then execute TWO queries. The first
     #   #for content types exclusive of Post and PostSection, the second query
     #   #only searches for JUST Post and PostSection using query dsl2. This is
     #   #because we need a specific, customized query just for the nested content
     #   #type PostSections and its parent, Post.
 
     #   (
-    #     (Elasticsearch::Model.search(dsl1, content_classes_without_post).records) + 
+    #     (Elasticsearch::Model.search(dsl1, content_classes_without_post).records) +
     #     (Elasticsearch::Model.search(dsl2, content_classes_just_post).records)
     #   ).to_a.sort_by {|obj| obj.published_at}.reverse!
 
@@ -227,45 +230,45 @@ class ContentFeedSearch
 end
 
 =begin
-  
+
   Context:
-  We've updated ElasticSearch to version 2.4 as of 3.19.17. This update resulted 
-  in some queries that originally pulled nested data no longer worked. In particular, 
-  it meant that searches for the content type Post broke the app - Post has_many PostSections, 
-  which, from ElasticSearch's perspective, is a nested content type of Post. 
-  The upgrade broke our nested content type queries. As such, we had to write some new queries 
+  We've updated ElasticSearch to version 2.4 as of 3.19.17. This update resulted
+  in some queries that originally pulled nested data no longer worked. In particular,
+  it meant that searches for the content type Post broke the app - Post has_many PostSections,
+  which, from ElasticSearch's perspective, is a nested content type of Post.
+  The upgrade broke our nested content type queries. As such, we had to write some new queries
   for contact_blog_search.rb and content_feed_search.rb
 
   How the New Queries Work
-  There are two new queries that work in conjunction with each other: dsl1 and dsl2. 
-  The first works by first pulling all content types the user wants. 
-  The second is a special query that pulls Post, and nested content of Post (PostSection). 
-  The code then combines the results of each query into an array, parses them, and presents 
-  them to the user. You can find these queries in models/contact_blog_search.rb and 
+  There are two new queries that work in conjunction with each other: dsl1 and dsl2.
+  The first works by first pulling all content types the user wants.
+  The second is a special query that pulls Post, and nested content of Post (PostSection).
+  The code then combines the results of each query into an array, parses them, and presents
+  them to the user. You can find these queries in models/contact_blog_search.rb and
   models/content_feed_search.rb. Take note that whether or not the first query looks
   for all content types, or just some, depends on a few factors you'll need to
   review in the code. Also take note that how queries are combined after they are
   made into arrays depends on a few factors, too. Again, please review the code.
 
   The Need for New ElasticSearch Rake Tasks
-  Unfortunately, our production cluster on Bonsai would not map our nested data fields for 
-  Post correctly. Of course, on staging, it do so without issue. Our work-around was to write 
-  special rake tasks that, using HTTParty, issue mapping, indexing, and importing POST commands 
-  directly to our Bonsai cluster. Previously, we relied on rake tasks that came bundled with our 
+  Unfortunately, our production cluster on Bonsai would not map our nested data fields for
+  Post correctly. Of course, on staging, it do so without issue. Our work-around was to write
+  special rake tasks that, using HTTParty, issue mapping, indexing, and importing POST commands
+  directly to our Bonsai cluster. Previously, we relied on rake tasks that came bundled with our
   various ElasticSearch gems.
 
   What the New Rake Tasks Are
-  There are two new rake tasks: create_post_index and custom_elasticsearch_import. 
-  If you need to import new data, use custom_elasticsearch_import. 
-  It will import data into all existing indices. If you need to destroy the 
-  Post or PostSection indices for any reason, or change their mappings (such as added a field), 
-  use/modify "create_post_index" accordingly. It will get the job done of creating and mapping 
+  There are two new rake tasks: create_post_index and custom_elasticsearch_import.
+  If you need to import new data, use custom_elasticsearch_import.
+  It will import data into all existing indices. If you need to destroy the
+  Post or PostSection indices for any reason, or change their mappings (such as added a field),
+  use/modify "create_post_index" accordingly. It will get the job done of creating and mapping
   the index on our Bonsai cluster the way you expect.
 
-  Note: It is critical that you understand how the rake tasks work - review them. 
-  If you have any questions, ask Brian, so he can pull in another dev who's worked on ES before. 
-  Also, if you want to experiment with POST requests to Bonsai clusters, 
-  use the interface provided by Bonsai. Go the cluster management dashboard, 
+  Note: It is critical that you understand how the rake tasks work - review them.
+  If you have any questions, ask Brian, so he can pull in another dev who's worked on ES before.
+  Also, if you want to experiment with POST requests to Bonsai clusters,
+  use the interface provided by Bonsai. Go the cluster management dashboard,
   and you'll see the option to do this. Of course, experiment on staging.
-  
+
 =end
