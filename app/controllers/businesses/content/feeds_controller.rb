511d098c0ca426dcbc695045a640bd8c06a177f6
Ryan Frisch <ryan@locable.com>
2019-05-01 16:01:23 -0700

    Fix filters in admin

--- app/controllers/businesses/content/feeds_controller.rb
+++ app/controllers/businesses/content/feeds_controller.rb
@@ -3,15 +3,15 @@ class Businesses::Content::FeedsController < Businesses::Content::BaseController
 
   def show
     if params[:unpublished]
-      @results = get_content(business: @business, query: params[:query], content_types: (params[:post_types].present? ? params[:post_types] : ['QuickPost', 'Post', 'BeforeAfter', 'Gallery', 'Offer', 'Job']), content_category_ids: params[:categories], content_tag_ids: params[:tags], page: params[:page], per_page: 20, published: false)
+      @results = get_content(business: @business, query: params[:query], content_types: (params[:post_types].present? ? params[:post_types].split : ['QuickPost', 'Post', 'BeforeAfter', 'Gallery', 'Offer', 'Job']), content_category_ids: (params[:categories].present? ? params[:categories].split : []), content_tag_ids: (params[:tags].present? ? params[:tags].split : []), page: params[:page], per_page: 20, published: false)
     elsif params[:published]
-      @results = get_content(business: @business, query: params[:query], content_types: (params[:post_types].present? ? params[:post_types] : ['QuickPost', 'Post', 'BeforeAfter', 'Gallery', 'Offer', 'Job']), content_category_ids: params[:categories], content_tag_ids: params[:tags], page: params[:page], per_page: 20, published: true)
+      @results = get_content(business: @business, query: params[:query], content_types: (params[:post_types].present? ? params[:post_types].split : ['QuickPost', 'Post', 'BeforeAfter', 'Gallery', 'Offer', 'Job']), content_category_ids: (params[:categories].present? ? params[:categories].split : []), content_tag_ids: (params[:tags].present? ? params[:tags].split : []), page: params[:page], per_page: 20, published: true)
     else
-      @results = get_content(business: @business, query: params[:query], content_types: (params[:post_types].present? ? params[:post_types] : ['QuickPost', 'Post', 'BeforeAfter', 'Gallery', 'Offer', 'Job']), content_category_ids: params[:categories], content_tag_ids: params[:tags], page: params[:page], per_page: 20, published: '')
+      @results = get_content(business: @business, query: params[:query], content_types: (params[:post_types].present? ? params[:post_types].split : ['QuickPost', 'Post', 'BeforeAfter', 'Gallery', 'Offer', 'Job']), content_category_ids: (params[:categories].present? ? params[:categories].split : []), content_tag_ids: (params[:tags].present? ? params[:tags].split : []), page: params[:page], per_page: 20, published: '')
     end
     @categories = @business.content_categories
     @tags = @business.content_tags
-    @post_types = %w(quick_post post before_after gallery offer job)
+    @post_types = %w(QuickPost Post BeforeAfter Gallery Offer Job)
     @graph = FacebookAnalytics.new(facebook_token: @business.facebook_token)
   end
 end
