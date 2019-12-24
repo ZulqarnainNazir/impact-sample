e2207b12fe340a8040b9ce92038e1cb4968abb0d
Matthew Dodds <matthewrusselldodds@gmail.com>
2019-12-24 16:37:32 -0500

    Remove configuration breaking assets for subdomain pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,6 @@ Rails.application.configure do
   config.eager_load = false
 
   config.action_controller.perform_caching = false
-  config.action_controller.asset_host = "http://#{Rails.application.secrets.host}"
-  config.action_mailer.asset_host = "http://#{Rails.application.secrets.host}"
   config.action_mailer.default_url_options = { host: Rails.application.secrets.host }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener
