c7382ea5dace6b3f5fbfbe56ed8df4fbc6a740a9
Ryan Frisch <ryan@locable.com>
2020-03-22 14:57:43 -0700

    Cleanup

--- app/controllers/businesses/data/alerts_controller.rb
+++ app/controllers/businesses/data/alerts_controller.rb
@@ -4,13 +4,7 @@ class Businesses::Data::AlertsController < Businesses::BaseController
   end
 
   def update
-
     update_resource @alert, alert_params, location: [:edit, @business, :data_alert]
-    # do |success|
-    #   if success
-    #     @alert.event_definitions.each(&:touch)
-    #   end
-    # end
   end
 
   private
