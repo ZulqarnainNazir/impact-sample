e42785c5018875a8d39a628f5607d933a2aed66e
Ryan Frisch <ryan@locable.com>
2020-03-24 12:57:15 -0700

    Scope cart items to business

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,14 +2,15 @@ class Cart < ActiveRecord::Base
   has_many :cart_items
   has_many :products, through: :cart_items
 
-  def cart_total
-    cart_items.includes(:product).map do |item|
+  def cart_total(business)
+    cart_items.includes(:product).joins(:product).where(products: {business_id: business.id}).map do |item|
       item.product.price * item.quantity
     end.reduce(:+) # map will put the multiplication of (quantity * Unit) into an array then reduce will sum them up
   end
 
-  def cart_item_count
-    cart_items.map do |item|
+  def cart_item_count(business)
+    # cart_items.map do |item|
+      cart_items.joins(:product).where(products: {business_id: business.id}).map do |item|
       item.quantity
     end.reduce(:+) # map will put the multiplication of (quantity * Unit) into an array then reduce will sum them up
   end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,7 @@
 class CartItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
+  # belongs_to :business
 
   validates :quantity, presence: true
   validates_numericality_of :quantity, :only_integer => true, :greater_than_or_equal_to => 1
